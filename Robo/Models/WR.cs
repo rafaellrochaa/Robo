63009046194948f40a76f8d100a816f306199911
Rafael <rafaell.rochaa@gmail.com>
2015-04-01 14:38:15 -0300

    commit alteracoes iniciais

--- Robo/Models/WR.cs
+++ Robo/Models/WR.cs
@@ -11,30 +11,6 @@ namespace Robo.Models
 {
     static public class WR
     {
-        static public string DoRequest(String url)
-        {
-            //cria uma requisição para a url
-            WebRequest rq = WebRequest.Create(url);
-
-            //obtem a resposta
-            HttpWebResponse rp = (HttpWebResponse)rq.GetResponse();
-            
-            //obtém um stream contendo a resposta retornada pelo servidor
-            Stream ds = rp.GetResponseStream();
-
-            //cria um StreamReader para leitura
-            StreamReader rd = new StreamReader(ds);
-
-            //LE O CONTEÚDO
-            string ct = rd.ReadToEnd();
-
-            rd.Close();
-            ds.Close();
-            rp.Close();
-
-            return ct;
-        }
-
         static public string DoPost(string urlGetAutenticacao, string UrlPost, NameValueCollection parametros)
         {
             using (var client = new CookieAwareWebClient())
