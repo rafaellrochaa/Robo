7744b69e7763994e726bea2564c43bc2e3caecc6
Rafael <rafaell.rochaa@gmail.com>
2015-04-01 11:38:50 -0300

    Primeiro commit

---
+++ Robo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Robo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Robo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public string Index()
+        {
+            string html = WR.DoRequest("http://www.dolarhoje.com.br/");
+            string expressao = "stockup stockup_bigger";
+
+            int posDolar = html.IndexOf(expressao);
+
+            return html.Substring(posDolar + expressao.Length + 2, 5);
+
+
+        }
+
+        public string Login()
+        {
+            var Credenciais = new NameValueCollection();
+
+            Credenciais.Add("usuario", "claudio");
+            Credenciais.Add("senha", "ac@78902");
+
+            return WR.DoPost("http://www.agilus.com.br/alvo", "http://www.agilus.com.br/alvo/Consultas", Credenciais);
+
+
+        }
+
+    }
+}
---
+++ Robo/Models/WR.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Robo.Models
+{
+    static public class WR
+    {
+        static public string DoRequest(String url)
+        {
+            //cria uma requisição para a url
+            WebRequest rq = WebRequest.Create(url);
+
+            //obtem a resposta
+            HttpWebResponse rp = (HttpWebResponse)rq.GetResponse();
+            
+            //obtém um stream contendo a resposta retornada pelo servidor
+            Stream ds = rp.GetResponseStream();
+
+            //cria um StreamReader para leitura
+            StreamReader rd = new StreamReader(ds);
+
+            //LE O CONTEÚDO
+            string ct = rd.ReadToEnd();
+
+            rd.Close();
+            ds.Close();
+            rp.Close();
+
+            return ct;
+        }
+
+        static public string DoPost(string urlGetAutenticacao, string UrlPost, NameValueCollection parametros)
+        {
+            using (var client = new CookieAwareWebClient())
+            {
+                var response = client.UploadValues(urlGetAutenticacao, parametros);
+                var result = client.DownloadData(UrlPost);
+                return Encoding.UTF8.GetString(result);
+            }
+        }
+
+        public class CookieAwareWebClient : WebClient
+        {
+            public CookieAwareWebClient()
+            {
+                CookieContainer = new CookieContainer();
+            }
+            public CookieContainer CookieContainer { get; private set; }
+
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                var request = (HttpWebRequest)base.GetWebRequest(address);
+                
+                request.CookieContainer = CookieContainer;
+                return request;
+            }
+        }
+
+    }
+}
